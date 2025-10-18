c9c67102847b160baa102fb9820c034d3a874c7b
a.volkov <a.volkov111555@gmail.com>
2025-10-19 00:30:44 +0300

    Laisy iterator of tasks was added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod task;
 mod range_or_once;
 
-use range_or_once::{Input, make_range_or_once};
 use itertools::iproduct;
+
+use task::Task;
+
+use range_or_once::{Input, make_range_or_once};
 
 fn main() {
     
-    let mut task = task::Task::new(60.0, 3.0, 0.08, 25.0, 27, 1.5);
+    let mut task = Task::new(60.0, 3.0, 0.08, 25.0, 27, 1.5);
 
     task.do_drop_while(5.0, 2.0, 0.9999);
 
@@ -23,6 +26,12 @@ fn main() {
     let mesh = get_meshgrid(3.0, 0.08, (20.0, 25.0, 0.5), (20.0, 31.0, 1.0), 1.5);
 
     for i in mesh {
+    println!("{:?}", i);
+    }
+
+    let tasks = get_tasks(60.0, 3.0, 0.08, (20.0, 25.0, 0.5), (20.0, 31.0, 1.0), 1.5);
+
+    for i in tasks {
     println!("{:?}", i);
     }
 }
@@ -43,4 +52,18 @@ fn get_meshgrid(
         make_range_or_once(alpha),
     )
 
+}
+
+fn get_tasks(
+    v_0: f64,
+    v_sec: impl Into<Input>,
+    v_def: impl Into<Input>,
+    flow: impl Into<Input>,
+    plate_count: impl Into<Input>,
+    alpha: impl Into<Input>,
+) -> impl Iterator<Item = Task> {
+    get_meshgrid(v_sec, v_def, flow, plate_count, alpha)
+        .map(move |(a, b, c, d, e)| {
+            Task::new(v_0, a, b, c, d as usize, e)
+        })
 }
