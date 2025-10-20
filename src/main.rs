b690fd148ccb8ba1e4ef2aeaa5d0c90056078d53
a.volkov <a.volkov111555@gmail.com>
2025-10-20 15:15:31 +0300

    Little cosmetic changes

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,17 @@ fn main() {
 
     let start = Instant::now();
     
-    let tasks = get_tasks(60.0, 3.0, (0.005, 0.015, 0.001), 25.0, 27.0, 1.5, (5.0, 18.0, 1.0), (0.01, 0.051, 0.01), 0.9999);
+    let tasks = get_tasks(
+        60.0,
+        3.0,
+        (0.005, 0.015, 0.001),
+        25.0,
+        27.0,
+        1.5, 
+        (5.0, 18.0, 1.0),
+        (0.01, 0.051, 0.01),
+        0.9999
+    );
 
     let best_task = tasks.max_by(|task1, task2| {
         let eff1 = task1.efficiency();
